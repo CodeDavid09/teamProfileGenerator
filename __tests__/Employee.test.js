ba059dc3bdcd26a4691cec8504cf1d079acbc7cf
David Gavino <ggavino2@uccs.edu>
2022-01-12 14:54:51 -0700

    completed employee and engineer test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,5 @@
+// https://dev.to/dstrekelj/how-to-write-unit-tests-in-javascript-with-jest-2e83 
+// documentation I used to help me create test.js files
 const Employee = require("./lib/employee");
 
 test("Can instantiate employee instance", () => {
@@ -6,7 +8,7 @@ test("Can instantiate employee instance", () => {
 });
 
 test("Can set name through constructor arguments", () => {
-    const name = "David"
+    const name = "Test"
     const e = new Employee(name);
     expect(e.name).toBe(name);
 });
