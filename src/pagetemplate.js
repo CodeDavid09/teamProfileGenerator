7ca0af528528099ce3400b4e2cbce8c6d9f59b65
David Gavino <ggavino2@uccs.edu>
2022-01-12 13:52:23 -0700

    added html

--- src/pagetemplate.js
+++ src/pagetemplate.js
@@ -12,7 +12,7 @@ const generateTeam = team => {
     </ul> 
     </div>
 </div>
-        ` 
+        `
     }
     const generateEngineer = engineer => {
         return `
@@ -77,7 +77,32 @@ const generateTeam = team => {
 
 module.exports = team => {
     return `
-
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- /* Font Awesome */ -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <!-- Google Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <!-- Bootstrap core CSS -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    
+        <link rel="stylesheet" type="css" href="">
+        <title>Team Profile Generator</title>
+    </head>
+    
+    <body>
+    <div class="row noborder justify-content-center" id="team-cards">
     ${generateTeam(team)} 
+    </body>
+
+    </html>
     `
 }
